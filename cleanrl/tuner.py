5aafd02ec23f3066ae8c7ac4fb0dd0cee619a0d0
kcorder <kcorder@udel.edu>
2022-06-26 21:50:02 -0400

    Add tuner.py for W&B sweeps

---
+++ cleanrl/tuner.py
@@ -0,0 +1,39 @@
+import argparse
+from distutils.util import strtobool
+from encodings import normalize_encoding
+from locale import normalize
+import runpy
+import time
+import numpy as np
+import sys
+import copy
+import wandb
+
+
+if __name__ == "__main__":
+    run_name = f"tuner_{int(time.time())}"
+    wandb.init(
+        project="cleanRL",
+        name=run_name,
+        monitor_gym=True,
+        save_code=True,
+    )
+    target_scores = {
+        # "HalfCheetah-v4": 8000,
+        # "Walker2d-v4": 6000,
+        # "Ant-v4": 6000,
+        "pong_v3": None
+    }
+    num_seeds = 3
+    main_args = copy.deepcopy(sys.argv)
+    scores = []
+    for env in target_scores.keys():
+        for seed in range(num_seeds):
+            sys.argv = main_args + ["--env-id", env, "--seed", str(seed)]
+            ppo = runpy.run_path(path_name='cleanrl/ppo_pettingzoo_ma_atari.py', run_name="__main__")
+            print(f"The average episodic return on {env} is {np.average(ppo['avg_returns'])}")
+            scores += [max(0, np.average(ppo['avg_returns']))]
+            # normalized_scores += [max(0, np.average(ppo['avg_returns']) / target_scores[env])]
+
+    print(f"The scores are {np.average(scores)}")
+    wandb.log({"scores": np.average(scores)})
